aa6b594d92a91e5a18460fac7cb6e351d2b40a17
Felix Serang <byte007@zoho.com>
2025-11-15 16:47:46 +0700

    ✨ feat: implement crud feature

--- mycv/src/users/user.entity.ts
+++ mycv/src/users/user.entity.ts
@@ -1,4 +1,4 @@
-import { Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, AfterInsert, AfterRemove, AfterUpdate } from "typeorm";
 
 @Entity()
 export class User {
@@ -10,4 +10,19 @@ export class User {
 
   @Column()
   password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log(`New user inserted with id: ${this.id}`);
+  }
+
+  @AfterUpdate()
+  logUpdate() {
+    console.log(`User updated with id: ${this.id}`);
+  }
+
+  @AfterRemove()
+  logRemove() {
+    console.log(`User removed with id: ${this.id}`);
+  }
 }
