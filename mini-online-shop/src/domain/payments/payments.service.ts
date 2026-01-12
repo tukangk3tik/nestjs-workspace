19fee682f37846f9abceb1668a1d2da8b438d4a0
Felix Serang <byte007@zoho.com>
2026-01-12 23:09:17 +0700

    implementing payment logic

--- mini-online-shop/src/domain/payments/payments.service.ts
+++ mini-online-shop/src/domain/payments/payments.service.ts
@@ -1,26 +1,43 @@
-import { Injectable } from '@nestjs/common';
-import { CreatePaymentDto } from './dto/create-payment.dto';
-import { UpdatePaymentDto } from './dto/update-payment.dto';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Payment } from './entities/payment.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Order } from '../orders/entities/order.entity';
+import { OrderStatus } from '../orders/enums/order-status.enum';
 
 @Injectable()
 export class PaymentsService {
-  create(createPaymentDto: CreatePaymentDto) {
-    return 'This action adds a new payment';
-  }
+  constructor(
+    @InjectRepository(Payment)
+    private readonly paymentRepository: Repository<Payment>,
+    @InjectRepository(Order)
+    private readonly orderRepository: Repository<Order>,
+  ) {}
 
-  findAll() {
-    return `This action returns all payments`;
-  }
+  async payOrder(id: number) {
+    const order = await this.orderRepository.findOne({
+      where: { id },
+      relations: {
+        payment: true,
+      },
+    });
 
-  findOne(id: number) {
-    return `This action returns a #${id} payment`;
-  }
+    if (!order) {
+      throw new NotFoundException('Order not found');
+    }
 
-  update(id: number, updatePaymentDto: UpdatePaymentDto) {
-    return `This action updates a #${id} payment`;
-  }
+    if (order.payment) {
+      throw new ConflictException('Order is already paid');
+    }
 
-  remove(id: number) {
-    return `This action removes a #${id} payment`;
+    const payment = this.paymentRepository.create();
+    order.payment = payment;
+    order.status = OrderStatus.AWAITING_SHIPMENT;
+
+    return this.orderRepository.save(order);
   }
 }
