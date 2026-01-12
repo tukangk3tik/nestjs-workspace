e9d362ce42673981ac942e5cca5f1b9e251e625f
Felix Serang <byte007@zoho.com>
2026-01-12 16:51:34 +0700

    add function overload for wrapping ids

--- mini-online-shop/src/common/util/id.util.ts
+++ mini-online-shop/src/common/util/id.util.ts
@@ -1,3 +1,15 @@
 import { IdDto } from '../dto/id.dto';
 
+export function wrapId(idOrIds: number): IdDto;
+export function wrapId(idOrIds: number[]): IdDto[];
+export function wrapId(idOrIds: number | number[]) {
+  if (Array.isArray(idOrIds)) {
+    const ids = idOrIds;
+    return ids.map((id) => ({ id }));
+  }
+
+  const id = idOrIds;
+  return { id };
+}
+
 export const idDtoIdentifier = (dto: IdDto) => dto.id;
