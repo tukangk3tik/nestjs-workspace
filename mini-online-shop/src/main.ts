2d13956814d6a64a095bf3e469bd12b319e7cece
Felix Serang <byte007@zoho.com>
2026-01-13 23:59:05 +0700

    adding helmet

--- mini-online-shop/src/main.ts
+++ mini-online-shop/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * Enable Cross-Origin Resource Sharing (CORS) to allow requests from different origins.
+   */
   app.enableCors();
+
+  /**
+   * Helmet helps you secure your Express apps by setting various HTTP headers.
+   */
+  app.use(helmet());
 
   const configService = app.get(ConfigService);
   const port = configService.get<number>('APP_PORT') || 3000;
